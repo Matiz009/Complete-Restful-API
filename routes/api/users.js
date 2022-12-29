350024eb6bdb48650cc15d2663f37f4d500c7162
MATI UL REHMAN <iammatiz00@gmail.com>
2022-12-29 12:10:57 +0500

    almost half completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+var bcrypt = require("bcryptjs");
+var _ = require("lodash");
 const { User } = require("../../models/user");
 router.post("/register", async(req, res) => {
     let user = await User.findOne({ email: req.body.email });
@@ -10,7 +12,21 @@ router.post("/register", async(req, res) => {
     user.name = req.body.name;
     user.email = req.body.email;
     user.password = req.body.password;
+    let salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    return res.send(user);
+    return res.send(_.pick(user, ["name", "email"]));
+});
+
+router.post("/login", async(req, res) => {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).send("Email not registered");
+    }
+    let isValid = await bcrypt.compare(req.body.password, user.password);
+    if (!isValid) {
+        return res.status(401).send("invalid Password");
+    }
+    res.send("Login Successfully!");
 });
 module.exports = router;
